61cfd59ef6cdbfdb905d1ca1c1844c00a1f31ad0
melvin <melvinsamuel070@github.com>
2024-07-17 15:36:14 +0100

    class_python

---
+++ variables.py
@@ -0,0 +1,138 @@
+# # This is for Varibales in python
+
+# # Declaring variable
+# ## This is a string data type
+# MyName = "Toothpick"
+# ## This is an integer data type
+# Age = 2
+# ## This for float data type
+# Age = 2.5
+# ## This is a boolean data type
+# IsTrue = True
+# ## List Data type
+# List = [1, 2, 3, 4, 5]
+# ## Tuples data type
+# Tuple = (1, 2, 3, 4, 5)
+# ## Set data type
+# Set = {1, 2, 3, 4, 5}
+# ## Dictionary data type
+# Dictionary = {"Name": "Toothpick", "Age": 2, "IsTrue": True}
+
+# ## Complex data type
+# Complex = 1 + 2j
+# cars = ("gip, wagone")
+# fruit = ("cashew, mango, orange")
+# name = "Abhishek"
+# age = 13
+# worth = 12.5
+# game = True
+# list_data = [1, 2, 3, 4, 5]
+# tuple_data = (1, 2, 3, 4, 5)
+# set_data = {1, 2, 3, 4, 5}
+# dict_data = {"Name": "Abhishek", "Age": 13, "Worth": 12.5}
+# data_complex = 13 + 12.5j
+# print (type(cars), type(fruit))
+# print (type(name), type(age))
+# print (type(worth), type(game))
+# print (type(list_data), type(tuple_data))
+# print (type(set_data), type(dict_data))
+# print (type(data_complex))
+
+# name = "Linux"
+# print (name[0])
+# print (name[1])
+# print (name[2])
+# print (name[3])
+# print (name[4])
+# Name = "James"
+# say = "Welcome Here! Your age is"
+# age = 10
+# print (Name, say, age)
+
+# age = 100.5
+# my_name = "Captain Dunkurk"
+# my_address = "Tijuana, Mexico"
+# my_address_num = 2+3j
+# print (age, my_name, my_address, my_address_num)
+
+# name = "John"
+# message = """
+# This is your new class on Python programming
+# I hope it get's to your medular oblaganta
+# Thank You!
+# """
+# print (name, message)
+
+# - Tuples are similar to list but they are immutable.
+# my_tuple = (1, 2, 3, 4, 5)
+# my_tuple[0] = 10
+# print (my_tuple)
+
+# list_name = ["Abhishek", "Ankit", "Ankur", "Ankush", "Anupam"]
+# list_name[0] = "Toothpick"
+# print (list_name)
+
+# ## for Dictionaries
+# person = {"Name": "Alice", "Age": 25, "Height": 175}
+# car = {'Brand': 'Toyota', 'Model': 'Corolla', 'Year': 2022}
+# print (person, car)
+
+#Sets
+# numbers = {1, 2, 3, 4, 5}
+# print (numbers)
+
+# name = int("Abhishek")
+# print (name)
+# name = 'Abhishek'  # Replace '123' with the actual numeric string you want to convert
+# num = int(name)
+
+# x = "Sally" # x is now of type str
+# y = int(3)# y will be 3
+# z = float(3) # z will be 3.0
+# print (x, y, z)
+# My_Variable_Name = {"chioma, adaobi, afolabi, kinsley, tunde"}
+# print (My_Variable_Name)
+# Di = {"Name": "Chioma", "Age": 34, "Model": 2008, "weight": 450}
+# print (Di)
+# x, y, z = "Orange", "Banana", "Cherry",
+# v, g, e = (1, 2, 3)
+# my_name = {"Name": "kikelomo", "Age": 20, "Level": 200, "Car": "Ranbogini",}
+# r=r = [ 200, 4000,  500, 6000]
+# ABCD = ("kikelomo")
+# string = str(ABCD) 
+# print (x, y, z)
+# print (v, g, e)
+# print (my_name)
+# print (string)
+# list_name = ["Abhishek", "Ankit", "Ankur", "Ankush", "Anupam"]
+# list_name[0] = "Toothpick"
+# list_name[3] = "helen"
+# print (list_name)
+
+# name = "devops is"
+# is_devops = True
+# print (name, is_devops)
+
+
+
+# def myfunc():
+#     y ="my sarvasion"
+#     print("jesus is " + y)
+
+#     y ="my personal, lord, and, saviour"
+#     print("and he is " + y)
+
+#     y = "my strenght "
+#     print ("he is " + y)
+
+# myfunc()
+
+
+# x = str(3)# x will be '3'
+# y = int(3)# y will be 3
+# z = float(3) # z will be 3.0
+# w = int("strin")
+# print (x)
+# print (y)
+# print (z)
+# print (w)
